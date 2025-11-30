72d6b8b9b93bd2ab042cb5aaed3d8ee85e695d69
willianpm <willianpm15@gmail.com>
2025-11-30 08:09:15 -0300

    fix: correct image selector and set alt attributes for profile images

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function toggleMode() {
   const html = document.documentElement;
   html.classList.toggle("light");
-  const img = document.querySelector("profile img");
+  const img = document.querySelector("#profile img");
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("alt", "Metalúrgica Fortaço Light");
   } else {
-    img.attributes("src", "./assets/avatar.png");
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "Metalúrgica Fortaço");
   }
 }
